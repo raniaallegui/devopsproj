6d364677c6efdcc66a17baa299711f4188a6085d
doniasarsar <Donia.sarsar@esprit.tn>
2023-11-07 18:20:14 +0100

    test

---
+++ src/test/java/tn/esprit/rh/achat/controllers/CategorieProduitControllerTest.java
@@ -0,0 +1,28 @@
+package tn.esprit.rh.achat.controllers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CategorieProduitControllerTest {
+
+    @Test
+    void getCategorieProduit() {
+    }
+
+    @Test
+    void retrieveCategorieProduit() {
+    }
+
+    @Test
+    void addCategorieProduit() {
+    }
+
+    @Test
+    void removeCategorieProduit() {
+    }
+
+    @Test
+    void modifyCategorieProduit() {
+    }
+}
